d1f63c8abeaff342cd70cb96c0187590f9e8f45f
Mazhorova <mazhorova1987@mail.ru>
2023-10-12 18:44:38 +0300

    update

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import './style.css'
+
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line') as HTMLElement
+document.querySelector('.slider-next')?.addEventListener('click', function(){
+  offset = offset + 450;
+  if (offset > 1800) {
+    offset = 0
+  }
+  sliderLine.style.left = -offset + "px"
+}
+
+)
+
+document.querySelector('.slider-prev')?.addEventListener('click', function(){
+  offset = offset - 450;
+  if (offset < 0) {
+    offset = 1800;
+  }
+  sliderLine.style.left = -offset + "px"
+}
+
+)
